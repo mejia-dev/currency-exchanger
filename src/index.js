a2106dd45ef72b109f01995478472ee11e0a9441
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-17 10:44:50 -0800

    Solve data formatting error using async function

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRates from './services/ExchangeRates.js';
 
+async function getExchangeData() {
+  const response = await ExchangeRates.getValidCodes();
+  if (response.supported_codes) {
+    console.log(response.supported_codes);
+  } 
+  else {
+    console.log("error");
+  }
+}
+
 function handleSampleForm() {
   event.preventDefault();
-  let currencies = {};
-  currencies = ExchangeRates.getValidCodesAsync();
-  console.log(currencies.documentation);
+  getExchangeData()
+  // let currencies = {};
+  // currencies = ExchangeRates.getValidCodesAsync();
+  // console.log(currencies.data);
   // document.getElementById("outputDiv").innerText = null;
   // const pTag = document.createElement("p");
   // pTag.append(document.getElementById("text-input").value);
--- src/services/ExchangeRates.js
+++ src/services/ExchangeRates.js
@@ -1,26 +1,9 @@
 export default class ExchangeRates {
-  static getValidCodes() {
-    // https://v6.exchangerate-api.com/v6/YOUR-API-KEY/codes
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`)
-      .then(function(response) {
-        if (!response.ok) {
-          const errorMessage = `${response.status} ${response.statusText}`;
-          throw new Error(errorMessage);
-        } else {
-          
-          let jsonResponse;
-          response.json().then((data) => jsonResponse = data);
-          return jsonResponse;
-        }
-      })      
-      .catch(function(error) {
-        return error;
-      });
-  }
-
-  static async getValidCodesAsync() {
+  static async getValidCodes() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`);
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`
+        );
       const jsonifiedResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}
