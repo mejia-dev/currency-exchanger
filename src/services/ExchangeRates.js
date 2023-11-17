91ecd9799af2a098ca896c663fceec2c6080a731
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-17 15:40:51 -0800

    Add event listener for error of convertCurrency. Still needs refinement

--- src/services/ExchangeRates.js
+++ src/services/ExchangeRates.js
@@ -1,20 +1,16 @@
 export default class ExchangeRates {
   static async getValidCodes() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes/amount`); 
-      window.alert("do we get here?");
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`); 
       const jsonResponse = await response.json();
       if (!(response.status === 200)) {
-        const errorMessage = `oops`;
-        // `${response.status} ${response.statusText}
-        // ${jsonResponse.message}`;
-        window.alert(errorMessage);
+        const errorMessage = `${response.status} ${response.statusText}
+        ${jsonResponse.message}`;
         throw new Error(errorMessage);
       }
       return jsonResponse;
     } 
     catch(error) {
-      window.alert("error here! " + error);
       return error;
     }
   }
@@ -22,14 +18,20 @@ export default class ExchangeRates {
   static convertCurrency(amount,currencyFrom,currencyTo) {
     return new Promise(function(resolve, reject) {
       let xhr = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${currencyTo}/${amount}`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${amount}/${currencyTo}`;
       xhr.addEventListener("loadend", function() {
         const response = JSON.parse(this.responseText);
         if (this.status === 200) {
           resolve(response);
         } else {
-          reject([this.status, this.statusText, this.message]);
+          reject(this.status);
         }
+      });
+
+      xhr.addEventListener("error", function() {
+        reject({
+          status: "test"
+        });
       });
       xhr.open("GET", url, true);
       xhr.send();
