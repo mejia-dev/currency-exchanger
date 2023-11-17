035cf53863c3098570015fa924864eaab5accdcb
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-17 13:03:28 -0800

    Add basic error handling for getValidCodes

--- src/index.js
+++ src/index.js
@@ -9,26 +9,26 @@ async function getConversionOptions() {
   const response = await ExchangeRates.getValidCodes();
   if (response.supported_codes) {
     addConversionOptions(response.supported_codes);
-  } 
+  }
   else {
-    printError("error");
+    processHTMLErrors(response.toString());
   }
 }
 
-function doConversion(amount,currencyFrom,currencyTo) {
-  let conversion = ExchangeRates.convertCurrency(amount,currencyFrom,currencyTo);
-  conversion.then(function(data) {
-    printResults(data.conversion_result,data.conversion_rate,data.base_code,data.target_code);
-  }, function(errorMessage) {
+function doConversion(amount, currencyFrom, currencyTo) {
+  let conversion = ExchangeRates.convertCurrency(amount, currencyFrom, currencyTo);
+  conversion.then(function (data) {
+    printResults(data.conversion_result, data.conversion_rate, data.base_code, data.target_code);
+  }, function (errorMessage) {
     printError(errorMessage);
   });
 }
 
 // UI Logic
-function addConversionOptions(array){
+function addConversionOptions(array) {
   const currencyFrom = document.getElementById("currencyFrom");
   const currencyTo = document.getElementById("currencyTo");
-  for(let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     let newFromOption = document.createElement("option");
     let newToOption = document.createElement("option");
     newFromOption.append(array[i]);
@@ -38,10 +38,24 @@ function addConversionOptions(array){
   }
 }
 
-function printResults(amount,rate,currencyFrom,currencyTo) {
+function printResults(amount, rate, currencyFrom, currencyTo) {
   document.getElementById("outputDiv").removeAttribute("class");
   document.getElementById("resultAmount").innerText = `${amount} ${currencyTo}`;
   document.getElementById("conversionRate").innerText = `1 ${currencyFrom} = ${rate} ${currencyTo}`;
+}
+
+function processHTMLErrors(errorInput) {
+  let errorOutput = "";
+  if (errorInput.includes("403")) {
+    errorOutput = "Error 403 Forbidden: Check to confirm that the API key is active and is entered correctly into the .env file";
+  } else if (errorInput.includes("404")) {
+    errorOutput = "Error 404 Not Found: Check to confirm that the API request was formatted correctly and try again";
+  } else if (errorInput.includes("5")) {
+    errorOutput = "Error 5xx Server Error: The API returned a server error. Please try again later.";
+  } else {
+    errorOutput = `Undefined ${errorInput}`;
+  }
+  printError(errorOutput);
 }
 
 function printError(error) {
@@ -52,20 +66,20 @@ function printError(error) {
 function handleConversionForm() {
   event.preventDefault();
   const amount = parseInt(document.getElementById("amount-input").value);
-  const currencyFrom = (document.getElementById("currencyFrom").value).substring(0,3);
-  const currencyTo = (document.getElementById("currencyTo").value).substring(0,3);
+  const currencyFrom = (document.getElementById("currencyFrom").value).substring(0, 3);
+  const currencyTo = (document.getElementById("currencyTo").value).substring(0, 3);
   if ((currencyFrom === "-Se") || (currencyTo === "-Se")) {
     printError('Please enter a valid currency');
     return;
-  } else if (currencyFrom === currencyTo ) {
-    printResults(amount,1,currencyFrom,currencyTo);
+  } else if (currencyFrom === currencyTo) {
+    printResults(amount, 1, currencyFrom, currencyTo);
     return;
   }
-  doConversion(amount,currencyFrom,currencyTo);
+  doConversion(amount, currencyFrom, currencyTo);
   printError('');
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   getConversionOptions();
   document.getElementById("conversion-form").addEventListener("submit", handleConversionForm);
 });
